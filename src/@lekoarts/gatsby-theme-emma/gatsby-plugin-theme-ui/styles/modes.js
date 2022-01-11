b5a51954383fcfb15a68325a49911ec997ca7eeb
gabrielba15 <gabriely101@hotmail.com>
2022-01-11 09:01:07 -0400

    ⚙️ config: 🆕 root colors. Context: changes root color.  Changes to be committed: 	modified:   src/@lekoarts/gatsby-theme-emma/gatsby-plugin-theme-ui/styles/modes.js 	modified:   src/@lekoarts/gatsby-theme-emma/gatsby-plugin-theme-ui/styles/styles.js

--- src/@lekoarts/gatsby-theme-emma/gatsby-plugin-theme-ui/styles/modes.js
+++ src/@lekoarts/gatsby-theme-emma/gatsby-plugin-theme-ui/styles/modes.js
@@ -1,10 +1,14 @@
-import { tailwind } from '@theme-ui/presets';
-
 export const modes = {
-	dark: {
-		text: tailwind.colors.white,
-		//primary: tailwind.colors.pink[4],
-		background: tailwind.colors.gray[8],
-		textMuted: tailwind.colors.gray[5],
-	},
+	dark: null,
 };
+
+// import { tailwind } from '@theme-ui/presets';
+
+// export const modes = {
+// 	dark: {
+// 		text: tailwind.colors.white,
+//		primary: tailwind.colors.pink[4],
+// 		background: tailwind.colors.gray[8],
+// 		textMuted: tailwind.colors.gray[5],
+// 	},
+// };
