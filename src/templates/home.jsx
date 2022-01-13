d91eef087dc8b7c26194c5ca868438da5fa9dc08
gabrielba15 <gabriely101@hotmail.com>
2022-01-13 12:20:25 -0400

    🧪 deploy testing v0.5.0 Context:     - Cards new border color set.     - New Info added.  Changes to be committed: 	modified:   src/components/cards/card.jsx 	modified:   src/templates/home.jsx

--- src/templates/home.jsx
+++ src/templates/home.jsx
@@ -68,17 +68,15 @@ const Home = () => {
 					}}
 				>
 					<Projects />
-					<CardVertical title="Gaboland">
+					<CardVertical title="Gaboland" borderColor="success">
 						My personal website or portfolio. This projects is built with
 						Gatsby, React and Theme UI.
 					</CardVertical>
-					<CardVertical title="Gaboland">
-						My personal website or portfolio. This projects is built with
-						Gatsby, React and Theme UI.
+					<CardVertical title="Giffy">
+						Gifs App using React, Styled Components and consuming giphy api.
 					</CardVertical>
-					<CardVertical title="Gaboland">
-						My personal website or portfolio. This projects is built with
-						Gatsby, React and Theme UI.
+					<CardVertical title="PHP & MYSQL Parcial" borderColor="success">
+						CRUD App using PHP, MySQL and Bootstrap for a university assessment.
 					</CardVertical>
 				</Flex>
 				<h2>Skills</h2>
@@ -90,17 +88,17 @@ const Home = () => {
 					}}
 				>
 					<Skills />
-					<CardVertical title="Gaboland">
-						My personal website or portfolio. This projects is built with
-						Gatsby, React and Theme UI.
+					<CardVertical title="Programming">
+						My life as a programmer started in 2019. I have 3 years of
+						experiences and I’m still learning.
 					</CardVertical>
-					<CardVertical title="Gaboland">
-						My personal website or portfolio. This projects is built with
-						Gatsby, React and Theme UI.
+					<CardVertical title="Design" borderColor="success">
+						I have 2 year designing and learning technologies associeted with it
+						and I still don’t stop.
 					</CardVertical>
-					<CardVertical title="Gaboland">
-						My personal website or portfolio. This projects is built with
-						Gatsby, React and Theme UI.
+					<CardVertical title="Autodidact">
+						since 2017 I haven’t stopped learning somenthing new every single
+						day.
 					</CardVertical>
 				</Flex>
 				<h2>Experiences</h2>
@@ -134,8 +132,16 @@ const Home = () => {
 					}}
 				>
 					<Contact />
-					<Card />
-					<Card />
+					<Card title="Work Contact">
+						If you are an entrepreneur or somebody who are looking for someone
+						who develop your app or website, you can contact me from 8:00h to
+						11:00h and from 15:00h to 18:h ET. for an immediate response.
+					</Card>
+					<Card title="Friendly Contact">
+						If you’re an old friend or a stranger who wants to contact me for
+						anything (say hello, connection, etc.) write at any tiime through
+						the media but do not expect an immediate response.
+					</Card>
 				</Flex>
 			</main>
 			<Footer />
