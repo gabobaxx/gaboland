9b81e7f98a3340f00e7d29aa4064999638133d18
gabrielba15 <gabriely101@hotmail.com>
2022-01-19 17:10:42 -0400

    🔄 refactor: 🆕 Refactor In Home Page. Context: Contact Card Component Added.  Changes to be committed: 	modified:   src/templates/home.jsx

--- src/templates/home.jsx
+++ src/templates/home.jsx
@@ -10,8 +10,8 @@ import useSiteMetadata from '@lekoarts/gatsby-theme-emma/src/hooks/use-site-meta
 // Third Party Components!
 import { Image, Card as CardUI, Flex } from 'theme-ui';
 // Custom Components!
-import ProjectCard from 'components/project-card.tsx';
-import Card, { CardVertical } from 'components/cards/card';
+import { CardVertical } from 'components/cards/card';
+import CardContact from 'components/cards/contact-card';
 import Seo from '@lekoarts/gatsby-theme-emma/src/components/seo';
 import Footer from '@lekoarts/gatsby-theme-emma/components/footer';
 import Header from '@lekoarts/gatsby-theme-emma/src/components/header';
@@ -24,7 +24,7 @@ import Projects from '../../content/sections/projects';
 import Experiences from '../../content/sections/experiences';
 
 const components = {
-	ProjectCard,
+	CardContact,
 };
 
 const Home = () => {
@@ -72,14 +72,19 @@ const Home = () => {
 				>
 					<Projects />
 					<CardVertical title="Gaboland" borderColor="success">
-						My personal website or portfolio. This projects is built with
-						Gatsby, React and Theme UI.
+						My personal website or portfolio. This projects is built
+						with Gatsby, React and Theme UI.
 					</CardVertical>
 					<CardVertical title="Giffy">
-						Gifs App using React, Styled Components and consuming giphy api.
+						Gifs App using React, Styled Components and consuming
+						giphy api.
 					</CardVertical>
-					<CardVertical title="PHP & MYSQL Parcial" borderColor="success">
-						CRUD App using PHP, MySQL and Bootstrap for a university assessment.
+					<CardVertical
+						title="PHP & MYSQL Parcial"
+						borderColor="success"
+					>
+						CRUD App using PHP, MySQL and Bootstrap for a university
+						assessment.
 					</CardVertical>
 				</Flex>
 				<h2>Skills</h2>
@@ -92,16 +97,16 @@ const Home = () => {
 				>
 					<Skills />
 					<CardVertical title="Programming">
-						My life as a programmer started in 2019. I have 3 years of
-						experiences and I'm still learning.
+						My life as a programmer started in 2019. I have 3 years
+						of experiences and I'm still learning.
 					</CardVertical>
 					<CardVertical title="Design" borderColor="success">
-						I have 2 year designing and learning technologies associeted with it
-						and I still don't stop.
+						I have 2 year designing and learning technologies
+						associeted with it and I still don't stop.
 					</CardVertical>
 					<CardVertical title="Autodidact">
-						since 2017 I haven't stopped learning somenthing new every single
-						day.
+						since 2017 I haven't stopped learning somenthing new
+						every single day.
 					</CardVertical>
 				</Flex>
 				<h2>Experiences</h2>
@@ -114,16 +119,16 @@ const Home = () => {
 				>
 					<Experiences />
 					<CardVertical title="Gaboland">
-						My personal website or portfolio. This projects is built with
-						Gatsby, React and Theme UI.
+						My personal website or portfolio. This projects is built
+						with Gatsby, React and Theme UI.
 					</CardVertical>
 					<CardVertical title="Gaboland">
-						My personal website or portfolio. This projects is built with
-						Gatsby, React and Theme UI.
+						My personal website or portfolio. This projects is built
+						with Gatsby, React and Theme UI.
 					</CardVertical>
 					<CardVertical title="Gaboland">
-						My personal website or portfolio. This projects is built with
-						Gatsby, React and Theme UI.
+						My personal website or portfolio. This projects is built
+						with Gatsby, React and Theme UI.
 					</CardVertical>
 				</Flex>
 				<h2>Contact</h2>
@@ -135,16 +140,27 @@ const Home = () => {
 					}}
 				>
 					<Contact />
-					<Card title="Work Contact">
-						If you are an entrepreneur or somebody who are looking for someone
-						who develop your app or website, you can contact me from 8:00h to
-						11:00h and from 15:00h to 18:h ET. for an immediate response.
-					</Card>
-					<Card title="Friendly Contact">
-						If you're an old friend or a stranger who wants to contact me for
-						anything (say hello, connection, etc.) write at any tiime through
-						the media but do not expect an immediate response.
-					</Card>
+					<CardContact
+						title="Work Contact"
+						icons={{ first: 'email', second: 'linkedin' }}
+					>
+						If you are an entrepreneur or somebody who are looking
+						for someone who develop your app or website, you can
+						contact me from 8:00h to 11:00h and from 15:00h to 18:h
+						ET. for an immediate response.
+					</CardContact>
+					<CardContact
+						title="Friendly Contact"
+						icons={{
+							first: 'telegram',
+							second: 'twitter',
+						}}
+					>
+						If you're an old friend or a stranger who wants to
+						contact me for anything (say hello, connection, etc.)
+						write at any time through the media but do not expect an
+						immediate response.
+					</CardContact>
 				</Flex>
 			</main>
 			<Footer />
