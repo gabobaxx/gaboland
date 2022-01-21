a5c731cd3ad0f2912cf001d55670bb9ae430a4c7
gabrielba15 <gabriely101@hotmail.com>
2022-01-21 16:03:27 -0400

    🔥 feature: 🆕 Component -> Badge. Context: Badge v1.  On branch develop  Changes to be committed: 	new file:   src/components/badges/badge.jsx 	modified:   src/components/cards/card.jsx

--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Card, Box } from 'theme-ui';
 
-const CardExperience = ({ _title, _borderColor }) => <h1>H</h1>;
 const CardSkill = ({ _title, _borderColor }) => <h1>H</h1>;
 const CardProject = ({ _title, _borderColor }) => <h1>H</h1>;
 export const SmallCard = ({ children, borderColor = 'p700' }) => (
