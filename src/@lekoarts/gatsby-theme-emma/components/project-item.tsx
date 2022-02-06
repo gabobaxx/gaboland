880524d0d73a43c17949b907d2efa10d952103df
gabrielba15 <gabriely101@hotmail.com>
2022-02-06 12:38:28 -0400

    🔥 feature: new projects layout. Context: grid layout to all projects template added.  On branch feature/page/single-project-template  Changes to be committed: 	new file:   content/projects/gaboland copy 2/index.mdx 	new file:   content/projects/gaboland copy 2/mountains.jpg 	new file:   content/projects/gaboland copy 3/index.mdx 	new file:   content/projects/gaboland copy 3/mountains.jpg 	new file:   content/projects/gaboland copy/index.mdx 	new file:   content/projects/gaboland copy/mountains.jpg 	new file:   content/projects/gaboland/index.mdx 	new file:   content/projects/gaboland/mountains.jpg 	new file:   content/projects/giffy/index.mdx 	new file:   content/projects/giffy/mountains.jpg 	modified:   src/@lekoarts/gatsby-theme-emma/components/project-item.tsx 	modified:   src/@lekoarts/gatsby-theme-emma/components/projects.tsx

--- src/@lekoarts/gatsby-theme-emma/components/project-item.tsx
+++ src/@lekoarts/gatsby-theme-emma/components/project-item.tsx
@@ -4,6 +4,8 @@ import { Link } from 'gatsby';
 import { animated } from 'react-spring';
 import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image';
 
+import { LargeProjectCard } from 'components/cards/project-card';
+
 type ProjectItemProps = {
 	node: {
 		color: string;
@@ -22,78 +24,21 @@ type ProjectItemProps = {
 };
 
 const ProjectItem = ({ node, style, eager }: ProjectItemProps) => (
-	<animated.div
-		sx={{
-			position: `relative`,
-			'&:before': {
-				content: `""`,
-				display: `block`,
-				paddingTop: `100%`,
-			},
-		}}
-		style={style}
-	>
+	<animated.div>
 		<div
 			sx={{
-				left: 0,
-				height: `100%`,
-				position: `absolute`,
-				top: 0,
-				width: `100%`,
 				a: {
-					color: `white`,
-					height: `100%`,
-					left: 0,
-					opacity: 0,
-					padding: 4,
-					position: `absolute`,
-					top: 0,
-					width: `100%`,
-					zIndex: 10,
+					width: 500,
 					transition: `all 0.3s ease-in-out`,
 					textDecoration: `none`,
 					'&:hover': {
-						color: `white`,
-						opacity: 1,
 						textDecoration: `none`,
 					},
 				},
 			}}
 		>
-			<div
-				sx={{
-					'> div': {
-						height: '100%',
-						left: 0,
-						position: `absolute`,
-						top: 0,
-						width: `100%`,
-						'> div': {
-							position: `static !important`,
-						},
-					},
-				}}
-			>
-				<GatsbyImage
-					loading={eager ? `eager` : `lazy`}
-					image={node.cover.childImageSharp.gatsbyImageData}
-					alt=""
-				/>
-			</div>
 			<Link to={node.slug} aria-label={`View detail page of ${node.title}`}>
-				<div
-					sx={{
-						backgroundColor: node.color,
-						height: `100%`,
-						left: 0,
-						position: `absolute`,
-						top: 0,
-						width: `100%`,
-						zIndex: -2,
-					}}
-				/>
-				<div sx={{ fontSize: 5, fontWeight: `bold` }}>{node.client}</div>
-				<div sx={{ fontSize: 2 }}>{node.service}</div>
+				<LargeProjectCard title={node.title} />
 			</Link>
 		</div>
 	</animated.div>
--- src/@lekoarts/gatsby-theme-emma/components/projects.tsx
+++ src/@lekoarts/gatsby-theme-emma/components/projects.tsx
@@ -21,7 +21,41 @@ type ProjectsProps = {
 	[key: string]: any;
 };
 
-// { projects }: ProjectsProps
+const ProjectsPlaceholderText = () => (
+	<Layout>
+		<Container>
+			<Themed.p>
+				Hi!{` `}
+				<span role="img" aria-label="Wave emoji">
+					👋
+				</span>
+				{` `}
+				<br />
+				Thanks for using <b>@lekoarts/gatsby-theme-emma</b>. You currently don't
+				have any content in your{` `}
+				<i>projects</i> folder - that's why this page displays a placeholder
+				text. Head over to the{` `}
+				<Themed.a href="https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-emma">
+					README
+				</Themed.a>
+				{` `}
+				to learn how to setup them.
+			</Themed.p>
+			<Themed.p>
+				<b>TL;DR:</b> <br />
+				The starter automatically created the folder{' '}
+				<code>content/projects</code>. Go into this folder, create a new folder
+				called <code>example</code> and create an <code>index.mdx</code> file
+				there and place an image. Edit the frontmatter like described in the
+				{` `}
+				<Themed.a href="https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-emma">
+					README
+				</Themed.a>
+				.
+			</Themed.p>
+		</Container>
+	</Layout>
+);
 
 const Projects = ({ projects }: ProjectsProps) => {
 	const trail = useTrail(projects.length, {
@@ -29,60 +63,31 @@ const Projects = ({ projects }: ProjectsProps) => {
 		to: { height: `100%` },
 	});
 
-	if (projects.length === 0) {
-		return (
-			<Layout>
-				<Container>
-					<Themed.p>
-						Hi!{` `}
-						<span role="img" aria-label="Wave emoji">
-							👋
-						</span>
-						{` `}
-						<br />
-						Thanks for using <b>@lekoarts/gatsby-theme-emma</b>. You currently
-						don't have any content in your{` `}
-						<i>projects</i> folder - that's why this page displays a placeholder
-						text. Head over to the{` `}
-						<Themed.a href="https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-emma">
-							README
-						</Themed.a>
-						{` `}
-						to learn how to setup them.
-					</Themed.p>
-					<Themed.p>
-						<b>TL;DR:</b> <br />
-						The starter automatically created the folder{' '}
-						<code>content/projects</code>. Go into this folder, create a new
-						folder called <code>example</code> and create an{' '}
-						<code>index.mdx</code> file there and place an image. Edit the
-						frontmatter like described in the{` `}
-						<Themed.a href="https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-emma">
-							README
-						</Themed.a>
-						.
-					</Themed.p>
-				</Container>
-			</Layout>
-		);
-	}
+	if (projects.length === 0) return <ProjectsPlaceholderText />;
 
 	return (
-		<Layout
-			sx={{
-				display: `grid`,
-				gridTemplateColumns: `repeat(auto-fit, minmax(280px, 1fr))`,
-				width: `100%`,
-			}}
-		>
-			{trail.map((style, index) => (
-				<ProjectItem
-					style={style}
-					eager={index === 0}
-					node={projects[index]}
-					key={projects[index].slug}
-				/>
-			))}
+		<Layout>
+			<main
+				sx={{
+					// Content Layout for par elements
+					display: 'grid',
+					gridTemplateColumns: 'repeat(auto-fit, minmax(366px, auto))',
+					gap: '1rem',
+					maxWidth: '90%',
+					margin: '24px auto',
+					alignItems: 'center',
+					justifyContent: 'center',
+				}}
+			>
+				{trail.map((style, index) => (
+					<ProjectItem
+						style={style}
+						eager={index === 0}
+						node={projects[index]}
+						key={projects[index].slug}
+					/>
+				))}
+			</main>
 		</Layout>
 	);
 };
