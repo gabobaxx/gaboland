ce2d7235837290c50339c75ccb250a4531749cd8
gabrielba15 <gabriely101@hotmail.com>
2022-09-26 20:06:51 -0400

    ⚙ config: making gatsby works

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,18 @@ import { Link } from '@remix-run/react';
 export default function Index() {
 	return (
 		<div className="mx-auto mt-16 max-w-7xl text-center">
-			<Link to="/posts" className="text-xl text-blue-600 underline">
-				Blog Posts
-			</Link>
+			<ul>
+				<li>
+					<Link to="/posts" className="text-xl text-blue-600 underline">
+						Blog Posts
+					</Link>
+				</li>
+				<li>
+					<a href="/gatsby" className="text-xl text-blue-600 underline">
+						Gaboland v1.0.0-beta
+					</a>
+				</li>
+			</ul>
 		</div>
 	);
 }
