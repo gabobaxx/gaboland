dea331a652ef94935983f02a8dab876f2a2bf73a
gabrielba15 <gabriely101@hotmail.com>
2022-10-13 17:05:14 -0400

    🔥 feature(hero): hero version 23 done

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,82 +1,78 @@
-import type { ReactNode } from 'react';
 import { Image, Box, Flex } from 'theme-ui';
 
 type HeroProps = {
-	children?: ReactNode;
+	image?: {
+		src: string;
+		alt?: string;
+	};
 	name?: string;
-	imageSrc?: string;
-	imageAlt?: string;
-	country?: string;
+	location?: string;
 	occupation?: string;
 };
-
-// mt: '1.5rem',
-// width: '100%',
-// gap: '0.75rem',
-// color: 'gs800',
-// display: 'flex',
-// textAlign: 'center',
-// flexDirection: 'column',
-
-const Hero = () => {
+const Hero = ({ image, name, location, occupation }: HeroProps) => {
 	return (
-		<Flex
-			sx={{
-				gridColumn: '1/-1',
-				alignItems: 'center',
-				flexDirection: 'column',
-			}}
-		>
+		<Flex variant="layout.hero">
 			<Image
 				as={Image}
-				alt="Selfie"
-				src="assets/me.jpg"
-				sx={{ borderRadius: '50%', width: 200, height: 200 }}
+				alt={image?.alt ?? 'Selfie'}
+				src={image?.src ?? 'assets/me.jpg'}
+				sx={{
+					width: 200,
+					height: 200,
+					borderRadius: '50%',
+				}}
 			/>
-			<h2>Gabriel Bencomo.</h2>
-			<Box as="h5">Web Developer & Solution Maker.</Box>
-			<Box as="h4">
-				<i style={{ marginRight: 5 + 'px' }} className="bi bi-geo-alt-fill"></i>
-				Venezuela
+			<Box sx={{ textAlign: 'center' }}>
+				<h2>{name ?? 'Jhon Doe'}</h2>
+				<h5>{occupation ?? 'Web Developer'}</h5>
+				{location && (
+					<h4>
+						<i
+							style={{ marginRight: 5 + 'px' }}
+							className="bi bi-geo-alt-fill"
+						></i>
+						{location}
+					</h4>
+				)}
 			</Box>
 		</Flex>
 	);
 };
 
-const HeroV1Beta = (props: HeroProps) => (
-	<Box as="section" sx={{ gridColumn: '1/-1' }}>
-		<Box sx={{ maxWidth: 200, mx: 'auto', mt: 60 }}>
-			<Image
-				as={Image}
-				src={props.imageSrc || 'assets/me.jpg'}
-				alt={props.imageAlt || 'Selfie'}
-				sx={{ borderRadius: '50%', width: '100%', height: 200 }}
-			/>
-			<Box
-				sx={{
-					mt: '1.5rem',
-					width: '100%',
-					gap: '0.75rem',
-					color: 'gs800',
-					display: 'flex',
-					textAlign: 'center',
-					flexDirection: 'column',
-				}}
-			>
-				<Box as="h2">Hi, I am {props.name || 'Gabriel'}.</Box>
-				<Box as="h5">{props.occupation || 'Software Engineer'}.</Box>
-				<Box as="h4">
-					<i
-						style={{ marginRight: 5 + 'px' }}
-						className="bi bi-geo-alt-fill"
-					></i>
-					{props.country || 'Venezuela'}
-				</Box>
-			</Box>
-		</Box>
+// const HeroV1Beta = (props: HeroProps) => (
+// 	<Box as="section" sx={{ gridColumn: '1/-1' }}>
+// 		<Box sx={{ maxWidth: 200, mx: 'auto', mt: 60 }}>
+// 			<Image
+// 				as={Image}
+// 				src={props.imageSrc || 'assets/me.jpg'}
+// 				alt={props.imageAlt || 'Selfie'}
+// 				sx={{ borderRadius: '50%', width: '100%', height: 200 }}
+// 			/>
+// 			<Box
+// 				sx={{
+// 					mt: '1.5rem',
+// 					width: '100%',
+// 					gap: '0.75rem',
+// 					color: 'gs800',
+// 					display: 'flex',
+// 					textAlign: 'center',
+// 					flexDirection: 'column',
+// 				}}
+// 			>
+// 				<Box as="h2">Hi, I am {props.name || 'Gabriel'}.</Box>
+// 				<Box as="h5">{props.occupation || 'Software Engineer'}.</Box>
+// 				<Box as="h4">
+// 					<i
+// 						style={{ marginRight: 5 + 'px' }}
+// 						className="bi bi-geo-alt-fill"
+// 					></i>
+// 					{props.country || 'Venezuela'}
+// 				</Box>
+// 			</Box>
+// 		</Box>
 
-		{props.children}
-	</Box>
-);
+// 		{props.children}
+// 	</Box>
+// );
 
 export default Hero;
