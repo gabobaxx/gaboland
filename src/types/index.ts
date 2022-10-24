a0ad3d72dfa228a773bbccdd52ce8b24ddc83f10
gabrielba15 <gabriely101@hotmail.com>
2022-10-24 17:31:00 -0400

    🔥 feature: new types and new icon classes

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,15 @@ export type NavLink = {
 	title: string;
 	slug: string;
 };
+
+export type AvailableIcons =
+	| 'cpu'
+	| 'star'
+	| 'laptop'
+	| 'article'
+	| 'yingyang'
+	| 'instagram'
+	| 'laptopFill'
+	| 'twitter'
+	| 'linkedin'
+	| 'github';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,8 @@
+import type { AvailableIcons } from 'types';
+
 export const linkedin = 'https://linkedin.com/in/gabrielbencomo';
 export const twitter = 'https://twitter.com/gabrielbca15';
 export const github = 'https://github.com/gabrielba15';
-
-type AvailableIcons =
-	| 'cpu'
-	| 'star'
-	| 'laptop'
-	| 'article'
-	| 'yingyang'
-	| 'instagram'
-	| 'laptopFill';
 
 export const getIconClass = (icon: AvailableIcons) => {
 	const IconClasses = {
@@ -20,6 +13,9 @@ export const getIconClass = (icon: AvailableIcons) => {
 		yingyang: 'bi bi-yin-yang',
 		instagram: 'bi bi-instagram',
 		laptopFill: 'bi bi-laptop-fill',
+		twitter: 'bi bi-twitter',
+		linkedin: 'bi bi-linkedin',
+		github: 'bi bi-github',
 	};
 
 	return IconClasses[icon];
