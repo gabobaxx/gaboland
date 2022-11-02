ef73c9f1b0dd1af6613dcaea3164f6187f15ff98
gabrielba15 <gabriely101@hotmail.com>
2022-11-02 12:24:25 -0400

    🔥 feature: contact component v2023

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,46 +1,44 @@
-import { Box, Flex } from 'theme-ui';
-import CardContact from 'components/cards/contact-card';
+import { capitalize } from 'utils';
+import { SocialLinks as links } from 'config';
 
-import { linkedin, twitter } from 'utils';
+import { Title, Body, Footer, ContactCard } from './card';
+
+const ContactInfo = [
+	{
+		title: 'Work Contact',
+		body: 'If you are an entrepreneur or somebody who are looking for someone who develop your app or website, you can contact me from 8:00h to 11:00h and from 15:00h to 18:h ET. for an immediate response.',
+		links: [links.linkedin, links.linkedin],
+		icons: ['inboxes-fill', 'linkedin'],
+	},
+	{
+		title: 'Friendly Contact',
+		body: 'If you are an old friend or a stranger who wants to contact me for anything (say hello, connection, etc.) write at any time through the media but do not expect an immediate response.',
+		links: ['t.me/gabrielba15', links.twitter],
+		icons: ['telegram', 'twitter'],
+	},
+];
 
 export default function Contact() {
 	return (
-		<Box as="section" id="contact">
-			<h2>Contact</h2>
-			<Flex
-				sx={{
-					gap: '1.5rem',
-					textAlign: 'center',
-					alignItems: 'center',
-					flexDirection: 'column',
-				}}
-			>
-				{/* <Contact /> */}
-				<CardContact
-					title="Work Contact"
-					icons={{ first: 'email', second: 'linkedin' }}
-					to={{
-						first: linkedin,
-						second: linkedin,
-					}}
-				>
-					If you are an entrepreneur or somebody who are looking for someone who
-					develop your app or website, you can contact me from 8:00h to 11:00h
-					and from 15:00h to 18:h ET. for an immediate response.
-				</CardContact>
-				<CardContact
-					title="Friendly Contact"
-					icons={{ first: 'telegram', second: 'twitter' }}
-					to={{
-						first: 'https://t.me/gabrielba15',
-						second: twitter,
-					}}
-				>
-					If you are an old friend or a stranger who wants to contact me for
-					anything (say hello, connection, etc.) write at any time through the
-					media but do not expect an immediate response.
-				</CardContact>
-			</Flex>
-		</Box>
+		<>
+			{ContactInfo.map((info) => (
+				<ContactCard key={info.title}>
+					<Title>{info.title}</Title>
+					<Body>{info.body}</Body>
+					<Footer>
+						<a target="_blank" href={info.links[0]} rel="noopener noreferrer">
+							<i className={`bi bi-${info.icons[0]}`}></i>
+							{info.icons[0] === 'inboxes-fill'
+								? 'Email'
+								: capitalize(info.icons[0])}
+						</a>
+						<a target="_blank" href={info.links[1]} rel="noopener noreferrer">
+							<i className={`bi bi-${info.icons[1]}`}></i>
+							{capitalize(info.icons[1])}
+						</a>
+					</Footer>
+				</ContactCard>
+			))}
+		</>
 	);
 }
