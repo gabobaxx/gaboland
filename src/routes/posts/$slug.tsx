8d9feec5ac6cb8b01ea3f1f10cef5926ffa6e4b9
gabrielba15 <gabriely101@hotmail.com>
2023-01-18 12:12:06 -0400

    fixing vercel problem #1

--- src/routes/posts/$slug.tsx
+++ src/routes/posts/$slug.tsx
@@ -14,33 +14,33 @@ import Header from 'components/header';
 type LoaderData = { post: Post; html: string };
 
 export const loader: LoaderFunction = async ({ params }) => {
-	invariant(params.slug, 'params.slug is required');
-	const post = await getPost(params.slug);
-	invariant(post, () => {
-		throw json(
-			{
-				route: 'posts',
-				slug: '/posts',
-			},
-			{
-				status: 404,
-				statusText: 'Post Not Found',
-			}
-		);
-	});
-
-	const html = marked(post.markdown);
-	return json<LoaderData>({ post, html });
+	// invariant(params.slug, 'params.slug is required');
+	// const post = await getPost(params.slug);
+	// invariant(post, () => {
+	// 	throw json(
+	// 		{
+	// 			route: 'posts',
+	// 			slug: '/posts',
+	// 		},
+	// 		{
+	// 			status: 404,
+	// 			statusText: 'Post Not Found',
+	// 		}
+	// 	);
+	// });
+	// const html = marked(post.markdown);
+	// return json<LoaderData>({ post, html });
 };
 
 export default function PostSlug() {
-	const { post, html } = useLoaderData() as LoaderData;
+	// const { post, html } = useLoaderData() as LoaderData;
 
 	return (
 		<main>
 			<Header />
-			<h1>{post.title}</h1>
-			<div dangerouslySetInnerHTML={{ __html: html }} />
+			{/* <h1>{post.title}</h1>
+			<div dangerouslySetInnerHTML={{ __html: html }} /> */}
+			POST
 		</main>
 	);
 }
