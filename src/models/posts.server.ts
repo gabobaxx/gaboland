720f90320e39afd77c143ef5c5f3d169277c405a
gabrielba15 <gabriely101@hotmail.com>
2023-02-14 11:13:02 -0400

    🔄 refactor: post type moved to types folder

--- src/models/posts.server.ts
+++ src/models/posts.server.ts
@@ -1,44 +1,29 @@
 import { Client } from '@notionhq/client';
-import type { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
+
+import type { Post, NotionResponse } from 'types';
 const { NotionToMarkdown } = require('notion-to-md');
 
 const NOTION_KEY = process.env.NOTION_KEY;
-const blogDatabaseId = process.env.NOTION_POSTS_DATABASE_ID || '';
 const notion = new Client({ auth: NOTION_KEY });
-
-export type Post = {
-	id: string;
-	slug: string;
-	title: string;
-	description: string;
-	markdown: string;
-	tags: [
-		{
-			id: string;
-			name: string;
-			color: string;
-		}
-	];
-	featured: boolean;
-};
+const blogDatabaseId = process.env.NOTION_POSTS_DATABASE_ID || '';
 
 export async function getPosts(): Promise<Array<Post>> {
 	const database = await notion.databases.query({
 		database_id: blogDatabaseId,
 	});
 
-	const posts = database.results as PageObjectResponse[];
+	const posts = database.results as NotionResponse;
 	return posts.map((post) => {
-		const tags = post.properties.Tags.multi_select;
-		const featured = tags.map((tag) => tag.name === 'Featured');
 		return {
 			id: post.id,
 			slug: post.id,
 			title: post.properties.Name.title[0].plain_text,
 			description: post.properties.Description.rich_text[0].plain_text,
 			markdown: '',
-			tags,
-			featured,
+			tags: post.properties.Tags.multi_select,
+			featured: post.properties.Tags.multi_select.map(
+				(tag) => tag.name === 'Featured'
+			),
 		};
 	});
 }
@@ -48,13 +33,13 @@ export async function getPost(slug: string) {
 
 	const page = (await notion.pages.retrieve({
 		page_id: slug,
-	})) as PageObjectResponse;
+	})) as any;
 
 	const mdblocks = await n2m.pageToMarkdown(page.id);
 	const mdString = n2m.toMarkdownString(mdblocks);
 
 	const tags = page.properties.Tags.multi_select;
-	const featured = tags.map((tag) => tag.name === 'Featured');
+	const featured = tags.map((tag: any) => tag.name === 'Featured');
 
 	const post: Post = {
 		id: page.id,
