665fa66029c3aabd9a29849c11416e420a9734e7
gabrielba15 <gabriely101@hotmail.com>
2024-05-19 01:02:23 -0400

    🔄 refactor: new class property added

--- src/components/cards/tailwind-card.tsx
+++ src/components/cards/tailwind-card.tsx
@@ -4,7 +4,7 @@ export default function TailwindCard(props: HTMLAttributes<HTMLDivElement>) {
 	return (
 		<div
 			{...props}
-			className={`max-w-sm min-h-52 p-6 bg-transparent border-2 border-gray-200 rounded-[1.25rem] ${
+			className={`w-full min-h-52 md:min-h-96 mx-auto p-6 bg-transparent border-2 border-gray-200 rounded-[1.25rem] ${
 				props.className ?? ''
 			}`}
 		/>
