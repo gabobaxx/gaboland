2c1bea3f7540395f4e367107d8b952959fcb4f09
gabrielba15 <gabriely101@hotmail.com>
2024-05-29 18:50:20 -0400

    🔥 feature: about page updated with new components

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -1,24 +1,27 @@
-import Header from 'components/header';
 import { PageNavLinks } from 'config';
 import VideoDemo from 'components/demo';
 import CallAgenda from 'components/agenda';
 import GPHero from 'components/hero/gp-hero';
 import TitleWithStarIcon from 'components/sections/title-with-star';
 import TailwindHeader from 'components/header/tailwind-header';
+import Footer from 'components/footer/tailwind-footer';
+import FooterSocialLinks from 'components/footer/tailwind-footer-social-links';
 
 export default function About() {
 	return (
 		<>
-			<Header links={PageNavLinks.about} />
 			<TailwindHeader links={PageNavLinks.about} />
-			{/* <main className="max-w-sm md:max-w-screen-md p-6 mx-auto">
+			<main className="max-w-sm md:max-w-screen-md p-6 mx-auto">
 				<VideoDemo />
 				<CallAgenda />
 				<GPHero />
 				<TitleWithStarIcon className="mt-6 justify-center items-center">
 					AGENDA UNA LLAMADA PARA IMPULSAR TU MARCA
 				</TitleWithStarIcon>
-			</main> */}
+				<Footer>
+					<FooterSocialLinks />
+				</Footer>
+			</main>
 		</>
 	);
 }
