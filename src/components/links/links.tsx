5b74133f17518664d5514ab93f474e09a152b29a
gabrielba15 <gabriely101@hotmail.com>
2024-07-23 12:07:03 -0400

    feat: external outline link

--- src/components/links/links.tsx
+++ src/components/links/links.tsx
@@ -14,3 +14,20 @@ export function Outline(props: AnchorHTMLAttributes<HTMLAnchorElement>) {
 		</Link>
 	);
 }
+
+export function ExternalOutline(
+	props: AnchorHTMLAttributes<HTMLAnchorElement>
+) {
+	return (
+		<a
+			{...props}
+			href={props.href}
+			target="_blank"
+			className={`w-full py-2.5 px-5 bg-transparent rounded-lg font-bold text-center text-sm focus:outline-none focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-grayscale-800 dark:border-grayscale-100 dark:text-grayscale-100 dark:hover:text-white dark:hover:bg-extra-dark dark:hover:border-grayscale-800 ${
+				props.className ?? ''
+			}`}
+		>
+			{props.children}
+		</a>
+	);
+}
