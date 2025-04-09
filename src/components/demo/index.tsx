9d5ac4da9aa39777662417ab49f681c4f31eb926
Gabriel from Gregory PC <gabriely101@hotmail.com>
2025-04-08 20:07:03 -0400

    feat: advertencia en about

--- src/components/demo/index.tsx
+++ src/components/demo/index.tsx
@@ -5,8 +5,7 @@ export default function TailwindCard(/*props: HTMLAttributes<HTMLDivElement>*/) 
 	return (
 		<>
 			<h1 className="text-lg md:text-3xl font-bold text-center">
-				LA TECNICA PROBADA DE 3 PASOS PARA DUPLICAR (X2) LOS CLIENTES DE UNA
-				MARCA DE TECNOLOGIA EN MENOS DE 3 MESES
+				ACTUALMENTE ESTAMOS TRABAJANDO (todavia no esta lista) EN LA TECNICA PROBADA DE 3 PASOS PARA DUPLICAR (X2) LOS CLIENTES DE UNA MARCA DE TECNOLOGIA EN MENOS DE 3 MESES
 			</h1>
 			<h2 className="text-sm md:text-sm font-bold text-center">
 				(SIN NECESIDAD DE GASTAR MILES DE DOLARES EN PUBLICIDAD Y MARKETING SIN
